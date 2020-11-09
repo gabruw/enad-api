98582dd38b03f2c534740279995d9a04a33c4475
gabruw <gabrielmarques.mg@gmail.com>
2020-11-09 20:52:20 -0300

    feat: Projeto Base

---
+++ src/main/java/com/uniacademia/enad/api/EnadApiApplication.java
@@ -0,0 +1,13 @@
+package com.uniacademia.enad.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniacademia/enad/api/EnadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniacademia.enad.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
